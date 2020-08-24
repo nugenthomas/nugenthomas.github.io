fbdbd8a2b3cc528db14f6089952f61f96f367c6a
nugenthomas <nguyn880@newschool.edu>
2020-08-23 22:29:09 -0400

    August 23rd Update

---
+++ .cache/sync-requires.js
@@ -0,0 +1,18 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/thomasnguyen/Documents/GitHub/nugenthomas.github.io.1/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/thomasnguyen/Documents/GitHub/nugenthomas.github.io.1/src/pages/404.js"))),
+  "component---src-pages-etsy-motion-guidelines-js": hot(preferDefault(require("/Users/thomasnguyen/Documents/GitHub/nugenthomas.github.io.1/src/pages/EtsyMotionGuidelines.js"))),
+  "component---src-pages-etsy-trust-js": hot(preferDefault(require("/Users/thomasnguyen/Documents/GitHub/nugenthomas.github.io.1/src/pages/EtsyTrust.js"))),
+  "component---src-pages-etsy-yir-js": hot(preferDefault(require("/Users/thomasnguyen/Documents/GitHub/nugenthomas.github.io.1/src/pages/EtsyYIR.js"))),
+  "component---src-pages-forecast-js": hot(preferDefault(require("/Users/thomasnguyen/Documents/GitHub/nugenthomas.github.io.1/src/pages/Forecast.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/thomasnguyen/Documents/GitHub/nugenthomas.github.io.1/src/pages/index.js"))),
+  "component---src-pages-instamo-js": hot(preferDefault(require("/Users/thomasnguyen/Documents/GitHub/nugenthomas.github.io.1/src/pages/Instamo.js"))),
+  "component---src-pages-roots-js": hot(preferDefault(require("/Users/thomasnguyen/Documents/GitHub/nugenthomas.github.io.1/src/pages/Roots.js")))
+}
+
